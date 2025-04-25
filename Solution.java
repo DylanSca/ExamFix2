a958fe9f4b83aeea0309dcf23682b01b958a208c
Dylan <rockrz91919@gmail.com>
2025-04-25 09:08:38 -0400

    Initial sharing of project

---
+++ Solution.java
@@ -0,0 +1,59 @@
+
+/**
+ * Write a description of class Solution here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Solution
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Solution
+     */
+    public Solution()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /*24(a)
+     * public void String(String a)
+     * a= tom
+     * a= Dan
+     * 
+     * (b)
+     * System.out.println(a);
+     * 
+     * (c)
+     * if(a=a)
+     * system.out.println(a+ "is equal");
+     * else
+     * (system.out.println(a+ "not equal");
+     * 
+     * 25)
+     * import.util.ArrayList
+     * public void tempatureCheck(){
+     *     ArrayList temp = New Arraylist;
+     *     if(temp<37.5){
+     *         return maximum;
+     }
+     }
+     * 26)collections.max final temp = maximum;
+     * 
+     * 27)We would use sort method to find the maximum tempature over 37.5 
+     *  and have them in a list, this can be done with the collections.sort in the code
+     * 
+     * 
+     */
+    
+    
+    
+    
+    
+    
+    
+    
+}

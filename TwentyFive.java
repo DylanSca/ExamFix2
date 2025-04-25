d655803a90f158ec2cbabb94ac61042e1e2b016c
Dylan <rockrz91919@gmail.com>
2025-04-25 09:48:00 -0400

    25 Unfinished

---
+++ TwentyFive.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class TwentyFive here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TwentyFive
+{
+   public TwentyFive(){
+       ArrayList temperatures = new ArrayList<Double>();
+       temperatures.add(34.5);
+       temperatures.add(39.5);
+       temperatures.add(44.5);
+       temperatures.add(24.5);
+       temperatures.add(44.5);
+       temperatures.add(38.5);
+       
+       fever(temperatures);
+   }
+    
+   public void fever(ArrayList<Double>temperatures){
+       int count= 0;
+       double max=0;
+       
+       System.out.println("Number of fever cases reported: " + count);
+       System.out.println("Higher fever case: " + max);
+   }
+    
+   public void getFeverAndMax(){
+       for(37.5 > temperatures
+           System.out.println(temperatures.add < 37.5);
+       }
+   }
+}
